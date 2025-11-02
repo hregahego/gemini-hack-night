75312db21a969e6034a7dc41dac4990229778575
Parth Mohan <parthmohan@Parths-MacBook-Pro-3.local>
2025-11-02 14:38:03 -0500

    fix bug in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,6 @@ export default function BlankTitlePage() {
     <div className="min-h-screen bg-gray-50 p-4">
       <div className="absolute top-4 left-4">
         <img src="https://i.postimg.cc/9QBwWZdR/image.png" alt="LOGO" width="144" height="144" />
-    <div className="min-h-screen bg-gray-50 p-4">
-      <div className="absolute top-4 left-4">
-        <img src="https://i.postimg.cc/9QBwWZdR/image.png" alt="LOGO" width="144" height="144" />
       </div>
       <div className="flex flex-col justify-center items-center min-h-screen gap-8">
         <h1 className="text-8xl font-extrabold text-green-600">
